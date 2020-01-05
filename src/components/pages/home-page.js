9a8f19653f7a24b9895472113100863a1dc1d7ef
Sergio <sergiiy@tallium.com>
2020-01-05 15:52:42 +0200

    setup read data from store

--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -2,16 +2,8 @@ import React from 'react';
 import BookList from '../book-list';
 
 const HomePage = () => {
-    const books = [
-        {
-            id: 1, title: 'The Dark Tower I: The Gunslinger', author: 'Stephen King'
-        },
-        {
-            id: 2, title: 'The Da Vinci Code', author : 'Den Brown'
-        }
-    ];
     return (
-        <BookList books={books} />
+        <BookList />
     )
 };
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,12 @@
 const initialState = {
-    books: []
+    books: [
+        {
+            id: 1, title: 'The Dark Tower I: The Gunslinger', author: 'Stephen King'
+        },
+        {
+            id: 2, title: 'The Da Vinci Code', author : 'Den Brown'
+        }
+    ]
 };
 
 const reducer = (state = initialState, action) => {
