742549b5280b62020850bb3a288d6ae36516f33b
Sergio <sergiiy@tallium.com>
2020-01-05 02:27:53 +0200

    setup redux components part 2

--- src/services/bookstore-services.js
+++ src/services/bookstore-services.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 
-export default class BookStore extends Component {
-    render() {
-        return []
+export default class BookStore  {
+    getBooks() {
+        return [
+            {
+                id: 1, title: 'The Dark Tower I: The Gunslinger', author: 'Stephen King'
+            },
+            {
+                id: 2, title: 'The Da Vinci Code', author : 'Den Brown'
+            }
+        ];
     }
-}
+};
