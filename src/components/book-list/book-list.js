25ef41629e5777ffdf9a2d4d626e99fa0f6c6b0b
Sergio <sergiiy@tallium.com>
2020-01-05 19:29:30 +0200

    added Promise

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -5,21 +5,22 @@ import { withBookStoreService } from '../HOC';
 import { booksLoaded } from '../../actions';
 import { compose } from '../../utils';
 import BookListItem from '../book-list-item';
+import Spinner from '../spinner';
 import './book-list.css';
 
 class BookList extends Component {
 
     componentDidMount() {
-        // 1. receive data
-        const { bookstoreService } = this.props;
-        const data = bookstoreService.getBooks();
-        console.log(data);
-        // 2. dispatch action to store
-        this.props.booksLoaded(data);
+        const { bookstoreService, booksLoaded } = this.props;
+        bookstoreService.getBooks()
+            .then((data) => booksLoaded(data));
     }
 
     render() {
-        const { books } = this.props;
+        const { books, loading } = this.props;
+        if (loading) {
+            return <Spinner/>
+        }
         return (
             <ul className='book-list'>
                 {
@@ -38,7 +39,8 @@ class BookList extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        books: state.books
+        books: state.books,
+        loading: state.loading,
     }
 };
 
