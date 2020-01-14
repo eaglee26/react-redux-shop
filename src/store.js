20cf62a9dd8bf0227a96f228824c09421dd7ec79
Sergii Yalovenko <sergiiy@tallium.com>
2020-01-14 15:54:33 +0200

    few enhancer

--- src/store.js
+++ src/store.js
@@ -2,22 +2,24 @@ import { createStore } from 'redux';
 
 import reducer from './reducers';
 
-const store = createStore(
-    reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const enhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+    const originalDispatch = store.dispatch;
+    store.dispatch = (action) => {
+        if (typeof action === 'string') {
+            return originalDispatch({
+                type: action
+            })
+        }
 
-const originalDispatch = store.dispatch;
-store.dispatch = (action) => {
-    if (typeof action === 'string') {
-        return originalDispatch({
-            type: action
-        })
-    }
+        return originalDispatch(action);
+    };
 
-    return originalDispatch(action);
+    return store;
 };
 
-store.dispatch('Hello pidar');
+const store = createStore(reducer, enhancer);
+
+//store.dispatch('Hello pidar');
 
 export default store;
